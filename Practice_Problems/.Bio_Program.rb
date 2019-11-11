20438e2c29066e2d38f2cd693c1b5b326e640827
Shonda Warren <norshonda.warren@my.ccsu.edu>
2019-11-11 18:43:26 -0500

    simple answer version with summary

---
+++ Practice_Problems/.Bio_Program.rb
@@ -0,0 +1,16 @@
+# Get user name
+ puts "What is your name?"
+ name = gets.chomp
+
+# Get Birthday
+puts "When is your birthday?"
+birthday = gets.chomp
+
+# What do they do for a living 
+puts "What is your Occupation?"
+Occupation = gets.chomp
+
+#Summary
+puts "Your name is " + name
+puts "And I am " + birthday
+puts " For a living I'm a " + Occupation
