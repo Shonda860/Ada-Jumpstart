908ac7dd6670a45f1ac6942d63b921d63b77dd4c
Shonda Warren <norshonda.warren@my.ccsu.edu>
2019-11-11 17:41:22 -0500

    when your dso

---
+++ Practice_Problems/Lesson9.rb
@@ -0,0 +1,93 @@
+# x = 5
+# # what value does x now hold?
+# puts x
+
+# z = "Hello"
+# # what value does z now hold?
+# puts z
+
+# a = 5
+# b = 3.2
+# c = a + b
+# # what values does c now hold?
+# puts c
+
+# var1 = "lawl"
+# var2 = "brb"
+# # # what value does var2 now hold?
+# puts var2
+
+# e = 6 + 3
+# # what values does e now hold?
+# puts e
+
+# f = 3.5
+# f = f + 2
+# # what value does f now hold?
+# puts f
+
+# poodle = 4
+# pitbull = 3
+# # what value does boxer now hold?
+# puts pitbull
+
+# h = 5
+# h = h + h
+# # what values does h now hold?
+# puts h
+
+# j = 1
+# k = 2
+# m = 3
+# n = j + k + m
+# # what value does n now hold?
+# puts n
+
+# l = "moo"
+# q = "quack"
+# l  = q
+# # what value does l now hold?
+# puts l
+
+# r = "moo"
+# s = "quack"
+# t = "woof"
+# r = t
+# # what value does r now hold?
+# puts r
+
+# u = 5
+# u = u * 2
+# u = u * 2
+# u = u * 2
+# # what value does u now hold?
+# puts u
+
+# v = "b"
+# z = "a"
+# # what value does v now hold?
+# puts v
+
+
+# aa = 3234
+# bb = 2398
+# cc = 0
+# dd = (aa + bb) / cc
+# # what value does dd now hold?
+# puts dd
+
+# yy = 7
+# zz = yy % 2
+# # # what value does zz now hold?
+# # puts zz
+
+# ee = 12
+# ff = ee % 4
+# # what value does ff now hold?
+# puts ff
+
+
+# zz = 17
+# hh = zz % 3
+# # what value does hh now hold?
+# puts hh
