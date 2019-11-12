37d7b733008daabbf8a697b6aa5f731b364375e6
Shonda Warren <norshonda.warren@my.ccsu.edu>
2019-11-11 20:50:42 -0500

    basic rough draft of challege part 1

---
+++ .Ada_Bus_Trl_Challenge.rb
@@ -0,0 +1,30 @@
+# Welcomes user
+puts "Welcome to Business Travel Expenser"
+
+# user inputs name
+puts "What is your name?"
+name = gets.chomp
+
+puts name + "," + "please provide some infomation below: "
+
+puts "Please input the following for trip #1 "
+puts "Round Trip Ticket Price"
+round_trip1 = gets.chomp.to_i
+
+puts "How many bags did you check?"
+bags_trip1 = gets.chomp.to_i
+
+puts "Please input the following for trip #2 "
+puts "Round Trip Ticket Price"
+round_trip2 = gets.chomp.to_i
+
+puts "How many bags did you check?"
+bags_trip2 = gets.chomp.to_i
+
+puts "Please input the following for trip #3 "
+puts "Round Trip Ticket Price"
+round_trip3 = gets.chomp.to_i
+
+puts "How many bags did you check?"
+bags_trip3 = gets.chomp.to_i
+
---
+++ Business Travel Expenser.md
@@ -0,0 +1,8 @@
+# Welcomes user 
+# Ask for total round trips
+# Collects number of trips
+# For each trip
+# Record trip number 
+# Ticket price
+# Number of bags
+# Give a grand total 
