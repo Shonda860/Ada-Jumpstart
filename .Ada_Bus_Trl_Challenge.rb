514d518d7fca440c8248e51894b70be28ac850ea
Shonda Warren <norshonda.warren@my.ccsu.edu>
2019-11-14 02:08:49 -0500

    test out if statements to compare cost

--- .Ada_Bus_Trl_Challenge.rb
+++ .Ada_Bus_Trl_Challenge.rb
@@ -1,144 +1,180 @@
-# Welcomes user
-puts "Welcome to Business Travel Expenser"
-
-#user enters thier name
- puts  "What is your name?"
- user_input = gets.chomp
-
-# Makes the first letter uppercase in name 
-name = user_input.capitalize
-
-#greets user
-  puts "#{name}, thank you let's get started."
-  
-#Round Trip 1
-puts "#{name},Please input the following for trip #1 "
-puts "Round Trip Ticket Price? " 
-first_ticket = gets.chomp.to_i
-number = gets.chomp
-a = number
-
-until number =~ /^-?[0-9]+$/ 
-  puts "Please enter a numerical vaule "
+ #welcomes user
+ greeting = "Welcome to Business Travel Expenser"
+ puts greeting.upcase
  
-  next unless number !=~ /^-?[0-9]+$/
-  puts "Try agrain"
-
- number= gets.chomp
-next number =~ /^-?[0-9]+$/
-
-end
-#number of bag
-puts "How many bags did you check? "
-first_bags = gets.chomp.to_f
-first_bags = first_bags * 50 
-
-
-# user enter accommodations cost 
-puts "Price of accommodations? "
-first_accom = gets.chomp.to_f
-
-# Grand total of Round trip 1
-first_total =  first_ticket + first_bags + first_accom
-first_total = '%.2f'% first_total
-puts "Your First Round Trip Cost a total of $" + first_total
-
-#Round Trip 2
-puts "#{name},Please input the following for trip #1 "
-puts "Rount Trip Ticket Price? "
-second_ticket = gets.chomp.to_f
+ # user enters thier name
+ # makes the first letter uppercase in name 
+ puts  "Please enter your name?"
+ user_input = gets.chomp
+ name = user_input.capitalize
+ 
+ #greets user
+ puts (name + ",thank you let's get started.")
+   
+ # user enter infomation about round trip 1
+ # displays hint if entry should be numerical
+ puts " Please input the following reguarding Roundtrip #1"
+ puts "Round Trip #1 Ticket Price? " 
+ first_ticket = gets.chomp
+ until first_ticket =~ /^-?[0-9]+$/ 
+   puts "Please enter a numerical vaule for cost of your flight. "
+   first_ticket= gets.chomp
+ next unless first_ticket =~ /^-?[0-9]+$/
+ end
   
-# user enter bag count Round Trip 2
-puts "How many bags did you check? "
-second_bags = gets.chomp.to_f
-second_bags = second_bags * 50 
-
-# user enter accommodations cost Round Trip 2 
-puts "Price of accommodations? "
-second_accom = gets.chomp.to_f
-
-# Grand total of Round trip 2
-second_total =  second_ticket + second_bags + second_accom
-second_total = '%.2f'% second_total
-puts "Your Second Round Trip Cost a total of $" + second_total
-
-# Round Trip 3
-puts "#{name},Please input the following for trip #3 "
-puts "Rount Trip Ticket Price? "
-third_ticket = gets.chomp.to_f
+ # enter info about bags on roundtrip 1
+ # displays hint if entry is not numerical
+ puts "How many bags did you check? "
+ first_bags = gets.chomp 
+ until first_bags =~ /^-?[0-9]+$/ 
+   puts "Number of bags must be numerical "
+   first_bags= gets.chomp
+ next unless first_bags =~ /^-?[0-9]+$/
+ end
+ 
+ # user enters accommodations cost roundtrip 1
+ # displays hint if entry is not numerical
+ puts "Price of accommodations? "
+ first_accom = gets.chomp
+ until first_accom =~ /^-?[0-9]+$/ 
+   puts "Please enter a numerical vaule for the cost of your accommodations."
+   first_accom = gets.chomp
+ next unless first_accom =~ /^-?[0-9]+$/
+ end
+ 
+ # Grand total of Round trip 1
+ first_total = '%.2f'% (first_ticket.to_f + first_bags.to_f * 50 + first_accom.to_f)
+ puts "Your First Round Trip cost a total of $" + first_total 
+ 
+ 
+ # user enter infomation about ticket round trip 2
+ # displays hint if entry should be numerical
+ puts (name + ",please input the following reguarding Roundtrip #2")
+ puts "Rount Trip #2 Ticket Price? "
+ second_ticket = gets.chomp
+ until second_ticket =~ /^-?[0-9]+$/ 
+   puts "Please enter a numerical vaule for cost of your flight. "
+   second_ticket= gets.chomp
+ next unless second_ticket =~ /^-?[0-9]+$/
+ end
+ 
+ # enter info about bags on roundtrip 2
+ # displays hint if entry is not numerical
+ puts "How many bags did you check? "
+ second_bags = gets.chomp
+ until second_bags =~ /^-?[0-9]+$/ 
+   puts "Number of bags must be numerical"
+   second_bags = gets.chomp
+ next unless second_bags =~ /^-?[0-9]+$/
+ end
+ 
+ # user enter accommodations cost Round Trip 2 
+ # displays hint if entry should be numerical
+ puts "Price of accommodations? "
+ second_accom = gets.chomp
+ until second_accom =~ /^-?[0-9]+$/ 
+   puts "Please enter a numerical vaule for the cost of your accommodations. "
+   second_accom = gets.chomp
+ next unless second_accom =~ /^-?[0-9]+$/
+ end
+ 
+ # Grandtotal of Round trip 2
+ second_total =  '%.2f' % (second_ticket.to_f + second_bags.to_f * 50 + second_accom.to_f)
+ puts "Your Second Round Trip cost a total of $" + second_total
+ 
+ # # Round Trip 3
+ puts "#{name},Please input the following for trip #3 "
+ puts "Round Trip Ticket Price? "
+ third_ticket = gets.chomp
+ until third_ticket =~ /^-?[0-9]+$/ 
+   puts "Please enter a numerical vaule for cost of your flight. "
+ third_ticket= gets.chomp
+ next unless third_ticket =~ /^-?[0-9]+$/
+ end
+ # # user enter bag count Round Trip 3
+ puts "How many bags did you check? "
+ third_bags = gets.chomp
+ until third_bags =~ /^-?[0-9]+$/ 
+   puts "Number of bags must be numerical"
+   third_bags= gets.chomp
+ next unless third_bags =~ /^-?[0-9]+$/
+ end
+ 
+ # user enters accommodations cost roundtrip 3
+ # displays hint if entry is not numerical
+ puts "Price of accommodations? "
+ third_accom = gets.chomp
+ until third_accom =~ /^-?[0-9]+$/ 
+   puts "Please enter a numerical vaule for the cost of your accommodations. "
+   third_accom= gets.chomp
+ next unless third_accom =~ /^-?[0-9]+$/
+ end
+ 
+ # grand total of Round trip 3
+ third_total =  '%.2f' % (third_ticket.to_f + third_bags.to_f * 50 + third_accom.to_f)
+ puts "Your Third Round Trip Cost a total of $" + third_total
+ 
+ # overall Grandtotal of all trips 
+ grand_cost = first_total.to_f + second_total.to_f + third_total.to_f
+ grand_cost = '%.2f'% grand_cost
+ puts "-----------------------------------------------------------------------------"
+ puts "Your grandtotal of all expenses of $ #{grand_cost}" "for all trips"  
+ puts "-----------------------------------------------------------------------------"
+ class Numeric
+   def percent_of(n)
+     self.to_f / n.to_f * 100.0
+   end
+ end
+ 
+ # total key
+ x = first_total.to_f .percent_of (grand_cost.to_f) 
+ y = second_total.to_f .percent_of (grand_cost.to_f)
+ z = third_total.to_f .percent_of (grand_cost.to_f) 
+ 
+ # puts " Trip _____ Cost_______ Percentage of Expenses"
+ summaryHash = {"Round Trip 1"=>["You spent a total of $" + first_total, + "which is ""#{"%.2f" %x}%" + "of your total expenses"],
+   "Round Trip 2"=>["You spent a total of $" + second_total, "which is ""#{"%.2f" %y}%" + "of your total expenses."], 
+   "Round Trip 3"=>["You spent a total of $" + third_total, + "which is ""#{"%.2f" %z}%" + "of your total expenses."]}
+ summaryHash.each do |a,b|
+   print " #{a} "
+   print "%4s %1s" % b
+   puts
+ end
+ 
+ # most expensive trip 
+ if (x > y) && (x > z)
+   puts "-----------------------------------------------------------------------------"
+   puts "Your most expensive trip was Trip 1 for a cost of #{first_total}"
+   puts "-----------------------------------------------------------------------------" 
+ elsif (y > x) && (y > z)
+   puts "------------------------------------------------------------------------------"
+   puts "Your most expensive trip was Trip 2 for a cost of #{second_total}"
+   puts "-------------------------------------------------------------------------------"
+ elsif (z > x) && (z > y)
+   puts "-------------------------------------------------------------------------------"
+   puts "Your most expensive trip was Trip 3 for a cost of $ #{third_total}"
+   puts "------------------------------------------------------------------------------"
+ elsif (x == y) && (x == z)
+   puts "-------------------------------------------------------------------------------"
+   puts "Your most expensive trips was Trip 1, Trip 2 and Trip 3 for a cost of #{third_total}"   
+   puts "------------------------------------------------------------------------------"
+ elsif (y == z)
+   puts "------------------------------------------------------------------------------"
+   puts "Your most expensive trip was Trip 2 and Trip 3 for a cost of #{third_total}" 
+   puts "------------------------------------------------------------------------------"
+ elsif (x == z)
+   puts "-------------------------------------------------------------------------------"
+   puts "Your most expensive trip was Trip 1 and Trip 3 for a cost of $ #{third_total}" 
+   puts "-------------------------------------------------------------------------------"
+ elsif (x == y)
+   puts "-------------------------------------------------------------------------------"
+   puts "Your most expensive trip was Trip 1 and Trip 2 for a cost of $ #{second_total}" 
+   puts "-------------------------------------------------------------------------------"
+ end
   
-# user enter bag count Round Trip 3
-puts "How many bags did you check? "
-third_bags = gets.chomp.to_f
-third_bags = third_bags * 50 
-
-# user enter accommodations cost Round Trip 3
-puts "Price of accommodations? "
-third_accom = gets.chomp.to_f
-
-# Grand total of Round trip 3
-third_total =  third_ticket + third_bags + third_accom
-third_total = '%.2f'% third_total
-puts "Your Third Round Trip Cost a total of $" + third_total
-
-# Overall Grandtotal of all trips 
-grand_cost = first_total.to_f + second_total.to_f + third_total.to_f
-grand_cost = '%.2f'% grand_cost
-
-puts  "Your grandtotal of all total roundtrips a total of $" + grand_cost
-
-class Numeric
-  def percent_of(n)
-    self.to_f / n.to_f * 100.0
-  end
-end
-
-#Note: the brackets () around number are optional
-a = first_total.to_f .percent_of (grand_cost.to_f) 
-b = second_total.to_f .percent_of (grand_cost.to_f)
-c = third_total.to_f .percent_of (grand_cost.to_f) 
-
-# puts " Trip _____ Cost_______ Percentage of Expenses"
-itemHash = {"Round Trip 1"=>["You spent a total of $" + first_total, + "which is ""#{"%.2f" %a}%" + "of your total expenses"],
-            "Round Trip 2"=>["You spent a total of $" + second_total, "which is ""#{"%.2f" %b}%" + "of your total expenses"], 
-            "Round Trip 3"=>["You spent a total of $" "which is "+ third_total, "#{"%.2f" %c}%" + "of your total expenses"]}
-            itemHash.each do |a,b|
-  print " #{a} "
-  print "%10s %2s" % b
-  puts
-end
-
-# #Trip 1 summary
-# first_total = '%.2f'% first_total
-# grand_cost = '%.2f'% grand_cost
-# puts "For trip 1 you spent a total $ #{"%.2f"% first_total.to_f} of or about #{"%.2f" %a}% of total expenses."
-
-# # Trip 2 summary
-# puts "For trip 2 you spent a total $ #{"%.2f"% second_total.to_f} of or about #{"%.2f" %b}% of total expenses."
-
-# # Trip 3  summary
-# puts "For trip 3 you spent a total $ #{"%.2f"% third_total.to_f} of or about #{"%.2f" %c}% of total expenses."
-
-# Most expensive trip 
-
-if (a > b) && (a > c)
-  puts "Your most expensive trip was Trip 1 for a cost of #{first_total}" 
-    elsif (b > a) && (b>c)
-      puts puts "Your most expensive trip was Trip 2 for a cost of #{second_total}"
-        elsif (c > a) && (c > b)
-          puts "Your most expensive trip was Trip 3 for a cost of $ #{third_total}"
-            elsif (a == b) 
-              puts "Your most expensive trip was Trip 1 for a cost of #{first_total}
-               and Your most expensive trip was Trip 2 for a cost of #{second_total}"    
-                elsif (b == c)
-                  "Your most expensive trip was Trip 2 for a cost of #{second_total} 
-                   and Your most expensive trip was Trip 3 for a cost of #{third_total}" 
-                    elsif (a == c)
-                    "Your most expensive trip was Trip 1 for a cost of #{first_total} 
-                    and Your most expensive trip was Trip 3 for a cost of #{third_total}" 
-  end
-  flights_total = first_ticket + second_ticket + third_ticket
-  accom_total = first_accom + second_accom + third_accom
- puts "You sent about $ #{'%.2f'% flights_total} on flights and $"  + "#{'%.2f'% accom_total} on accommodations"
-
-
+ 
+ # total of all flights and accommodations 
+  flights_total = '%.2f'% (first_ticket.to_f + second_ticket.to_f + third_ticket.to_f)
+ accom_total = '%.2f'% (first_accom.to_f + second_accom.to_f + third_accom.to_f)
+ puts "You spent about $ #{'%.2f'% flights_total} on flights and $"  + "#{'%.2f'% accom_total} on accommodations"
+ puts "--------------------------------------------------------------------------------"
